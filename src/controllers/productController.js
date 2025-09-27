be1001e15bce5325517f7b3bcb6192ca31163a86
Mayank mehta <mayankmehta1327@gmail.com>
2025-09-27 12:12:13 +0530

    Add product controller with CRUD endpoints

---
+++ src/controllers/productController.js
@@ -0,0 +1,99 @@
+const productService = require('../services/productService');
+
+// Get all products
+const getAllProducts = async (req, res, next) => {
+  try {
+    const result = await productService.getAllProducts(req.query);
+    
+    res.status(200).json({
+      success: true,
+      count: result.products.length,
+      pagination: result.pagination,
+      data: result.products
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get single product
+const getProduct = async (req, res, next) => {
+  try {
+    const product = await productService.getProductById(req.params.id);
+    
+    res.status(200).json({
+      success: true,
+      data: product
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Create product
+const createProduct = async (req, res, next) => {
+  try {
+    const product = await productService.createProduct(req.body);
+    
+    res.status(201).json({
+      success: true,
+      message: 'Product created successfully',
+      data: product
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Update product
+const updateProduct = async (req, res, next) => {
+  try {
+    const product = await productService.updateProduct(req.params.id, req.body);
+    
+    res.status(200).json({
+      success: true,
+      message: 'Product updated successfully',
+      data: product
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Delete product
+const deleteProduct = async (req, res, next) => {
+  try {
+    await productService.deleteProduct(req.params.id);
+    
+    res.status(200).json({
+      success: true,
+      message: 'Product deleted successfully'
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get low stock products
+const getLowStockProducts = async (req, res, next) => {
+  try {
+    const products = await productService.getLowStockProducts();
+    
+    res.status(200).json({
+      success: true,
+      count: products.length,
+      data: products
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getAllProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getLowStockProducts
+};
