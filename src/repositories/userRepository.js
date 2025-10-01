862ac9c7ff351fcefbf0dd638df8a28cca53f57f
Mayank mehta <mayankmehta1327@gmail.com>
2025-10-01 23:34:28 +0530

    fine future improv, add comment

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -4,7 +4,7 @@ class UserRepository {
   // Find user by email
   async findByEmail(email, includePassword = false) {
     if (includePassword) {
-      return User.findOne({ email }).select('+password');
+      return User.findOne({ email }).select('+password'); // Service doesn't know about select('+password') - that's Mongoose-specific
     }
     return User.findOne({ email });
   }
@@ -20,4 +20,9 @@ class UserRepository {
   }
 }
 
-module.exports = new UserRepository();
+module.exports = new UserRepository(); // Singleton Pattern 
+
+// Future needs: Will you need:
+// List all users (admin panel)?
+// Update user profile?
+// Soft delete (mark inactive instead of delete)?
