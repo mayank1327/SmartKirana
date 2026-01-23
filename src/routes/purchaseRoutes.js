443be3d03d96071188ca571f91d97296e25ba666
Mayank mehta <mayankmehta1327@gmail.com>
2026-01-23 10:03:58 +0530

    updating routes for purchase

--- src/routes/purchaseRoutes.js
+++ src/routes/purchaseRoutes.js
@@ -3,32 +3,29 @@ const {
   createPurchase,
   getPurchases,
   getPurchase,
-  updatePaymentStatus,
-  getPendingPayments,
-  getSupplierSummary,
-  getPurchaseAnalytics,
   getTodaysPurchases
 } = require('../controllers/purchaseController');
 const { protect } = require('../middleware/auth');
 const validate = require('../middleware/validate');
-const purchaseValidator = require('../validators/purchaseValidator');
+const {
+  createPurchaseSchema,
+  getPurchasesQuerySchema,
+  purchaseIdParamSchema
+} = require('../validators/purchaseValidator');
 
 const router = express.Router();
 
 // Protect all routes
 router.use(protect);
 
-// Quick access routes (before parameterized routes)
-router.get('/today', getTodaysPurchases);
-router.get('/supplier-summary', getSupplierSummary);
-router.get('/analytics', getPurchaseAnalytics);
-
-// CRUD routes
+// Purchase routes
 router.route('/')
-  .get(validate(purchaseValidator.getPurchasesQuerySchema, 'query'),getPurchases)
-  .post(validate(purchaseValidator.createPurchaseSchema), createPurchase);
+  .get(validate(getPurchasesQuerySchema, 'query'), getPurchases)
+  .post(validate(createPurchaseSchema), createPurchase);
 
-router.route('/:id').get(getPurchase);
+router.get('/today', getTodaysPurchases);
 
+router.route('/:purchaseId')
+  .get(validate(purchaseIdParamSchema, 'params'), getPurchase);
 
 module.exports = router;
