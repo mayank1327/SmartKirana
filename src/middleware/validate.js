1368941b2959352dbf5b22828f4b9e698f9ecc88
Mayank mehta <mayankmehta1327@gmail.com>
2026-03-15 15:59:01 +0530

    validate middleware adding comment

--- src/middleware/validate.js
+++ src/middleware/validate.js
@@ -1,22 +1,26 @@
-// Generic validation middleware
 const validate = (schema, property = 'body') => (req, res, next) => {
+
     const { error, value } = schema.validate(req[property], { 
       abortEarly: false ,
     });
   
     if (error) {
-      // Return all errors in one response
-      const messages = error.details.reduce((acc, detail) => { // refinement
-        acc[detail.path[0]] = detail.message; 
+      const messages = error.details.reduce((acc, detail) => {
+        acc[detail.path.join('.')] = detail.message;
         return acc;
       }, {});
-      return res.status(400).json({ success: false, errors: messages });
+      return res.status(400).json({
+        success: false,
+        error: messages
+      });
     }
   
-    // Replace req.body with validated value
     req[property] = value; // Controller receives CLEANED data automatically!
-
     next();
-  };
+    
+};
   
-  module.exports = validate;
+module.exports = validate;
+
+
+
