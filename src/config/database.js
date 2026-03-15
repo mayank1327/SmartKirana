f97d87f87968bd753c6e685f69c38aa2eb2e1bbb
Mayank mehta <mayankmehta1327@gmail.com>
2026-03-15 15:47:55 +0530

    clean and simlple connection to db

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,8 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('MongoDB connected successfully from database.js');
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
-    process.exit(1);
-  }
+const connectDB = async (uri) => {
+  await mongoose.connect(uri);
+  console.log('MongoDB connected successfully');
 };
 
 module.exports = connectDB;
