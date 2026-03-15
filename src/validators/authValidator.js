70056b1f06d2d536538a8ddf104e5697a91f379c
Mayank mehta <mayankmehta1327@gmail.com>
2026-03-15 16:01:17 +0530

    update password message in common

--- src/validators/authValidator.js
+++ src/validators/authValidator.js
@@ -1,31 +1,25 @@
 const Joi = require('joi');
 
-// Better approach : // FOLLOW DRY PRINCIPLE
+// FOLLOW DRY PRINCIPLE
 const commonValidations = {
-  email: Joi.string().email().required().messages({
+  email: Joi.string().trim().lowercase().email().required().messages({
     'string.email': 'Invalid email address',
     'string.empty': 'Email is required'
   }),
-  password: Joi.string().min(6).required().messages({
-    'string.min': 'Password must be at least 6 characters',
+  password: Joi.string().min(8).required().messages({
+    'string.min': 'Password must be at least 8 characters',
     'string.empty': 'Password is required'
   })
 };
 
-// Registration validation
 const registerSchema = Joi.object({
   name: Joi.string().trim().required().messages({
     'string.empty': 'Valid Name is required to register'
   }),
   email: commonValidations.email,
   password: commonValidations.password,
-  role: Joi.string().valid('owner', 'staff', 'manager').required().messages({
-    'any.only': 'Role must be one of owner, staff, manager',
-    'string.empty': 'Role is required'
-  })
 });
 
-// Login validation
 const loginSchema = Joi.object({
   email: commonValidations.email,
   password:commonValidations.password
