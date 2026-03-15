8d8ab6f7645c36c1dffb61d983ce8d72fb05a63e
Mayank mehta <mayankmehta1327@gmail.com>
2026-03-15 16:09:32 +0530

    add app error, remove noise comment,manage response

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,22 +1,20 @@
 const  generateToken = require('../utils/jwt');
-const userRepository = require('../repositories/userRepository'); // Import the user repository
-// Service orchestrates both to complete business operation -> depends on buth utils and repo 
+const userRepository = require('../repositories/userRepository');
+const AppError = require('../utils/AppError');
+
 class AuthService {
-  // Register a new user
-  async registerUser({ name, email, password, role }) {
 
-    // No duplicate emails
-    const existingUser = await userRepository.findByEmail(email); // Check if user already exists
+  async registerUser({ name, email, password }) {
 
-    if (existingUser) { //TODO: Better to use custom error classes in real apps
-      const error = new Error('User already exists');
-      error.status = 400; // or 409 (conflict)
-      throw error;
+    const existingUser = await userRepository.findByEmail(email);
+
+    if (existingUser) { 
+        throw new AppError('User already exists', 400);
     }
 
-    const user = await userRepository.create({ name, email, password, role }); // Create new user
+    const user = await userRepository.create({ name, email, password }); 
 
-    const token = generateToken(user._id); // Generate JWT token
+    const token = generateToken(user._id);
 
     return {
       token,
@@ -24,47 +22,32 @@ class AuthService {
         id: user._id,
         name: user.name,
         email: user.email,
-        role: user.role
       }
     };
   }
 
-  // Login existing user
   async loginUser({ email, password }) {
 
-    const user = await userRepository.findByEmail(email, true); // Include password for comparison
+    const user = await userRepository.findByEmail(email, true);
+    // Include password for comparison
 
+    
     if (!user || !(await user.comparePassword(password))) { 
-     // TODO : Better to use custom error classes in real apps
-      const error = new Error('Invalid credentials');
-      error.status = 401; // or 409 (conflict)
-      throw error;
-    }
+        throw new AppError('Invalid credentials', 401);
+    }// Same error for both cases — prevents user enumeration attack
 
-    const token = generateToken(user._id); // Generate JWT token
+    const token = generateToken(user._id);
 
     return {
       token,
-      data: { // ALTERNATIVE -> DTOs (Data Transfer Objects) for shaping response
+      data: { // DTOs (Data Transfer Objects) for shaping response
         id: user._id,
         name: user.name,
         email: user.email,
-        role: user.role
       }
     };
   }
+  
 }
 
 module.exports = new AuthService();
-
-
-// Future: Will you need:
-// Password reset?
-// Email verification?
-// OAuth/social login?
-// Update profile
-// Account deletion?
-// Refresh tokens?
-// Two-factor authentication?
-// Role-based access control?
-// etc...
