0089e2534fdf2174b75657b9aa061cea815a210b
Mayank mehta <mayankmehta1327@gmail.com>
2026-03-15 15:55:04 +0530

    adding app error

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,59 +1,32 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
 const config = require('../config');
+const AppError = require('../utils/AppError');
 
 const protect = async (req, res, next) => {
   try {
     let token;
 
-    // Check for token in headers
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
       token = req.headers.authorization.split(' ')[1];
     }
 
     if (!token) {
-      return res.status(401).json({
-        success: false,
-        error: 'Not authorized to access this route'
-      });
+      throw new AppError('Not authorized to access this route', 401);
     }
 
-    // Verify token
     const decoded = jwt.verify(token, config.JWT_SECRET);
-    
-    // Find user
-    const user = await User.findById(decoded.id);
-    if (!user) {
-      return res.status(401).json({
-        success: false,
-        error: 'No user found with this token'
-      });
-    }
+    req.user = { _id: decoded.id };
+    next(); 
 
-    req.user = user;
-    next();
   } catch (error) {
-    return res.status(401).json({
-      success: false,
-      error: 'Not authorized to access this route'
-    });
+    if (error.name === 'TokenExpiredError') {
+      return next(new AppError('Token expired, please login again', 401));
+    }
+    if (error instanceof AppError) {
+      return next(error);
+    }
+    return next(new AppError('Not authorized to access this route', 401));
   }
 };
 
-// Role-based authorization
-const authorize = (...roles) => {
-  return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return res.status(403).json({
-        success: false,
-        error: `User role ${req.user.role} is not authorized to access this route`
-      });
-    }
-    next();
-  };
-};
-
-module.exports = {
-  protect,
-  authorize
-};
+module.exports = { protect };
